35f1e313d4ecd52d43a6cfdb085cc96f99957e29
flawedworld <flawedworld@flawed.world>
2021-09-29 04:37:18 +0100

    Use APNs and CarrierConfig from APV

--- bramble.mk
+++ bramble.mk
@@ -3,3 +3,5 @@ $(call inherit-product, device/google/bramble/aosp_bramble.mk)
 
 # Remove AOSP prefix from product name
 PRODUCT_NAME := bramble
+# Tell build system not to bundle sample APNs from AOSP
+OEM_APNS := true
