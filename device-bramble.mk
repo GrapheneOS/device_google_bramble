66514b5f7758267d0b39e45e4dab772211c49aa9
flawedworld <flawedworld@flawed.world>
2022-04-13 20:02:34 +0100

    Add aggregated sysconfig xml with items from stock OS

--- device-bramble.mk
+++ device-bramble.mk
@@ -211,3 +211,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     NfcOverlayBramble
+
+# sysconfig XML from stock
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml
